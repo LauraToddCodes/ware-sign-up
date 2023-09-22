daba72632c3072fec08609b1bb63b172c9614e39
LauraToddCode <laura20todd@gmail.com>
2023-09-22 17:57:43 +0100

    Add background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}"],
+	content: ["./src/**/*.{html,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				sm: "375px",
+				lg: "1440px",
+			},
+		},
 	},
 	plugins: [],
 }
