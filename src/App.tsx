8db9b5f0b9d1b589e8402fba576e97abaf08fbc3
LauraToddCode <laura20todd@gmail.com>
2023-09-23 17:54:07 +0100

    Add padding to background

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 	return (
 		<>
 			<div className="absolute inset-0 bg-red"></div>
-			<div className="bg-[url('../public/bg-intro-mobile.png')] sm:bg-[url('../public/bg-intro-desktop.png')] min-h-screen absolute inset-0">
+			<div className="bg-[url('../public/bg-intro-mobile.png')] sm:bg-[url('../public/bg-intro-desktop.png')] min-h-screen absolute inset-0 pt-24 pb-16 px-6 sm:pt-16 sm:px-40">
 				<p>hello world</p>
 			</div>
 		</>
