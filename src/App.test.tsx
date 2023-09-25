a5ccbf31b11f3e73ef224f1137cfc46273e405c3
LauraToddCode <laura20todd@gmail.com>
2023-09-25 17:52:10 +0100

    Add JumboText

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import { render, screen } from "@testing-library/react"
 
 import App from "./App"
-import React from "react"
+import { Content as JumboContent } from "./components/jumboText/jumboText.content"
+test("renders Jumbo Text", () => {
+	const { JumboHeader, JumboParagraph } = JumboContent
 
-test("renders hello world", () => {
+	render(<App />)
+	const h1Header = screen.getByRole("heading")
+	expect(h1Header).toHaveTextContent(JumboHeader)
+	const paragraph = screen.getByTestId("jumbo-paragraph")
+	expect(paragraph).toHaveTextContent(JumboParagraph)
+})
 	render(<App />)
 	const placeholderText = screen.getByText(/hello world/i)
 	expect(placeholderText).toBeInTheDocument()
